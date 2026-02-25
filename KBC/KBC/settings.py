12ffa9d58e56006792a27c168ca3305efb253e08
Apoorav Mukherjee <apooravmukherjee@gmail.com>
2026-02-25 11:16:57 +0530

    Updated Settings for Deployment Ease

--- KBC/KBC/settings.py
+++ KBC/KBC/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-1o4^7286ll16-vib-v65$mwv6cyf7nyhb8-=bf)d_qiud1#-ct
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 
