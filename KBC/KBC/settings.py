eb5db41688fab380e8102e5aa6c5a33d3fb42ce3
Apoorav Mukherjee <apooravmukherjee@gmail.com>
2026-02-25 11:53:42 +0530

    Deploy ready Final

--- KBC/KBC/settings.py
+++ KBC/KBC/settings.py
@@ -77,12 +77,19 @@ WSGI_APPLICATION = 'KBC.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
-}
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
@@ -124,6 +131,7 @@ STATIC_URL      = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / 'staticfiles' ]
 STATIC_ROOT     = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Auth redirects
 LOGIN_URL          = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
